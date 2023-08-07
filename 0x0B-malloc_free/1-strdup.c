28dc821167adc2045fbea94f098a01402bcd165d
famkhan006 <famkhan006@gmail.com>
2023-08-07 05:57:12 -0700

    duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,28 +9,28 @@
  */
 char *_strdup(char *src)
 {
-    char *duplicate;
-    unsigned int i = 0;
+	char *duplicate;
+	unsigned int i = 0;
 
-    if (src == NULL)
-        return NULL;
+	if (src == NULL)
+		return (NULL);
 
-    while (src[i] != '\0')
-        i++;
+	while (src[i] != '\0')
+		i++;
 
-    duplicate = malloc(sizeof(char) * (i + 1));
+	duplicate = malloc(sizeof(char) * (i + 1));
 
-    if (duplicate == NULL)
-        return NULL;
+	if (duplicate == NULL)
+		return (NULL);
 
-    i = 0;
-    while (src[i] != '\0')
-    {
-        duplicate[i] = src[i];
-        i++;
-    }
+	i = 0;
+	while (src[i] != '\0')
+	{
+		duplicate[i] = src[i];
+		i++;
+	}
 
-    duplicate[i] = '\0';
+	duplicate[i] = '\0';
 
-    return duplicate;
+	return (duplicate);
 }
