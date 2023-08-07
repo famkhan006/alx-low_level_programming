152e2d3b5b986d998bcb371763f468befbe6e127
famkhan006 <famkhan006@gmail.com>
2023-08-07 11:51:32 -0700

    I am Fahmi

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,13 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *) malloc(width * sizeof(int));
-	
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			for (j = 0; j <= i; j++)
+				free(arr[j]);
+			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
