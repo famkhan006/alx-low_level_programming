bb3079707e62a7809a095041572d4e9caa2b3abe
famkhan006 <famkhan006@gmail.com>
2023-08-07 04:51:41 -0700

    checking if comments are necessary

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+char *create_array(unsigned int size , char c);
+
+char *_strdup(char *str);
+
+char *str_concat(char *s1 , char *s20);
+
+int **alloc_grid(int wifth , int height);
+
+char *argstostr(int ac , char **av);
+
+char **strtow(char *str);
+
+#endif
